a86efc8ef4dd0a579a4229ac34c598e15df28c2a
Peter O'Connor <sunnyflunk@gmail.com>
2020-08-03 08:56:04 +1000

    Create and mount image

--- image-gen.sh
+++ image-gen.sh
@@ -3,6 +3,9 @@
 # Build in RAM for testing
 LABEL="Serpent"
 WORKDIR="/tmp/${LABEL}ISO"
+ROOTDIR="${WORKDIR}/ROOT"
+IMAGE_SIZE="5GB"
+IMG="${WORKDIR}/IMG/${LABEL}.img"
 executionPath=$(dirname $(realpath -s $0))
 
 if [ `id --user` != 0 ]; then
@@ -12,6 +15,16 @@ fi
 # Import used functions
 . ${executionPath}/common/functions.sh
 
-[[ -d $WORKDIR ]] && rm -rf $WORKDIR
+requireTools fallocate mkfs.ext4 tune2fs
 
-mkdir -p $WORKDIR
+[[ -d ${WORKDIR} ]] && rm -rf ${WORKDIR}
+
+mkdir -p ${WORKDIR}/IMG
+mkdir -p ${ROOTDIR}
+
+# Prepare blank image
+fallocate -l ${IMAGE_SIZE} ${IMG} || serpentFail "Unable to create image"
+mkfs.ext4 -F ${IMG} || serpentFail "Unable to format image"
+tune2fs -c0 -i0 ${IMG}
+
+mount -o loop -t auto ${IMG} ${ROOTDIR} || serpentFail "Unable to mount image to ${ROOTDIR}"
